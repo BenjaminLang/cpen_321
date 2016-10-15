0213ebfe70af4d5a9e0ac692a41999c104ca58dd
Benjamin <benjaminlang12@gmail.com>
2016-10-14 18:03:59 -0700

    merge

--- Server/query.py
+++ Server/query.py
@@ -1,15 +1,13 @@
 import socket
 
-def query():
-    sock = socket.socket()
-    host = socket.gethostname()
-    port = 6969
+class query():
+    def __init__(self, ):
+        self.sock = socket.socket()
+        self.host = socket.gethostname()
+        self.port = 6969
 
-    sock.connect((host, port))
-    sock.send(b"SELECT * FROM books LIMIT 3")
-    print('client recieved: ',  repr(sock.recv(1024)))
-    sock.close()
-
-
-if __name__ == '__main__':
-    query()
+    def start_query(self):
+        self.sock.connect((self.host, self.port))
+        self.sock.send()
+        print('client recieved: ',  repr(self.sock.recv(1024)))
+        self.sock.close()
