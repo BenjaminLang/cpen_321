b66f0b9f58e25314cddd0db288654b1116698168
JSun12 <jsun.1207@gmail.com>
2016-10-21 23:47:42 -0700

    searchable activity

---
+++ Android/SmartShopper/app/src/main/java/androidapp/smartshopper/smartshopper/SearchableActivity.java
@@ -0,0 +1,42 @@
+package androidapp.smartshopper.smartshopper;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+public class SearchableActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //search(query);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        SearchManager searchMan = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        //searchView.setOnQueryTextListener(this);
+        searchView.setSearchableInfo(searchMan.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        return true;
+    }
+}
