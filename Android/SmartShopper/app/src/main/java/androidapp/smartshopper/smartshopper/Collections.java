ef954eb4e947af0f31d2ed3561abd82091029882
BenjaminLang <benjaminlang12@gmail.com>
2016-10-23 01:30:26 -0700

    idk

--- Android/SmartShopper/app/src/main/java/androidapp/smartshopper/smartshopper/Collections.java
+++ Android/SmartShopper/app/src/main/java/androidapp/smartshopper/smartshopper/Collections.java
@@ -3,11 +3,9 @@ package androidapp.smartshopper.smartshopper;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.ArrayList;
 
 /**
