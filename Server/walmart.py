2add1e007bfd27b2ac779bbdedcd9654ec1e5b51
Ryan Liu <Ryan Liu>
2016-11-02 23:25:52 -0700

    Updated walmart.py to work with Python 3

--- Server/walmart.py
+++ Server/walmart.py
@@ -5,7 +5,7 @@ import json
 
 # Parses starting from the base_url and sends the data to the db
 def parse():
-    full_json = json.load(urlopen('http://api.walmartlabs.com/v1/taxonomy?format=json&apiKey=' + api_key))
+    full_json = json.loads(urlopen('http://api.walmartlabs.com/v1/taxonomy?format=json&apiKey=' + api_key).read().decode('utf8'))
 
     departments = full_json['categories']
 
@@ -22,7 +22,7 @@ def parse():
                 cat_id = subcat['id']
                 cat_name = subcat['name']
 
-                cat_json = json.load(urlopen('http://api.walmartlabs.com/v1/paginated/items?format=json&category=' + cat_id + '&apiKey=' + api_key))
+                cat_json = json.loads(urlopen('http://api.walmartlabs.com/v1/paginated/items?format=json&category=' + cat_id + '&apiKey=' + api_key).read().decode('utf8'))
                 items = cat_json['items']
 
                 for item in items:
