693d01401525df840581dcbb3b28f7df5305554e
Ubuntu <ryangroup@vindos.xedcnqc3iptehl54jctq1mz4gf.dx.internal.cloudapp.net>
2016-11-12 01:57:01 -0800

    added music to walmart exclusions

--- Server/walmart.py
+++ Server/walmart.py
@@ -5,7 +5,7 @@ import json
 
 # Parses starting from the base_url and sends the data to the db
 def parse():
-    deps_exclusions = {'91083', '5426', '4096'}
+    deps_exclusions = {'91083', '5426', '4096', '4104'}
     full_json = json.loads(urlopen('http://api.walmartlabs.com/v1/taxonomy?format=json&apiKey=' + api_key).read().decode('utf8'))
 
     departments = full_json['categories']
