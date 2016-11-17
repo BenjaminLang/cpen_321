73897dd02cc9fef28376b8b3935074239e582c54
Amrit Kooner <amrit_k729@hotmail.com>
2016-11-17 02:56:02 -0800

    done superstore, needs integrating

--- Server/superstore.py
+++ Server/superstore.py
@@ -8,70 +8,27 @@ categories_db = client.categories
 item_db = client.items
 users_db = client.users
 
-# take in a soup, tag_name, class_name and return all "a href" links in that soup
-def _get_links(soup, tag_name, class_name):
-    links = list()
-    for tag in soup.find_all(tag_name, class_name):
-        for tagLink in tag.find_all('a'):
-            links.append(tagLink['href'])
-    return links
+# Takes in a soup and sends the product to the db (including price, url, name, image)
+def _send_product(prod, cat_item, url):
+    data = {}
+    for img in prod.find_all('img'):
+        data['name'] = img['alt']
+        data['image'] = img['src']
+    for button in prod.find_all('button'):
+        data['price'] = strip_name(str(button), 'Price:', 'EA').strip().replace('\n', '')
 
-# Takes a soup and sends all products to the db (including price, url, name, image)
-def _send_products(soup, cat_item):
-    print soup
+    data['store'] = 'Superstore'
+    data['url'] = url
+    print(data)
     return
-    for prod in soup.find_all('div', 'product-cell quickview-button'):
-        print prod
-        return
-        #TODO: add rest of item and send to db
-        if prod_info:
-            info = prod_info[0]
-            print info
-            return
-            url = thumbnail['href']
-            image = ''
-            list_img = thumbnail.find_all('img')
 
-            if list_img:
-                img = list_img[0]
-                if img.has_attr('src'):
-                    image = img['src']
-                elif img.has_attr('data-src'):
-                    image = img['data-src']
+    send_to_db(cat_item, data, categories_db, item_db, users_db)
 
-            list_caption = thumbnail.find_all('div', 'caption')
-            if list_caption:
-                caption = list_caption[0]
-                list_price = caption.find_all('div', 'price')
-                if list_price:
-                    price = str(list_price[0]).split('$')[1].split('</')[0]
-                else:
-                    continue  # no price for this item
-
-                list_description = caption.find_all('p', 'description')
-                if list_description:
-                    name = str(list_description[0]).split('>')[1].split('<')[0]
-                else:
-                    continue  # no name for this item
-            else:
-                continue  # no info for this item
-
-            data = {}
-            name.encode('ascii', 'ignore')
-            data['name'] = name.replace('.', '-').lower()
-            data['price'] = price
-            data['url'] = url
-            data['image'] = image
-            data['store'] = 'Save on Foods'
-
-            send_to_db(cat_item, data, categories_db, item_db, users_db)
     return
 
 # Parses starting from the base_url and sends the data to the db
 def parse():
     soup = get_soup(base_url)
-    #food_soup = get_soup(base_url + '/food')
-    #home_soup = get_soup(base_url + '/homenLifestyle')
 
     set_links = set()
     for cat in soup.find_all('li'):
@@ -81,15 +38,8 @@ def parse():
 
     for link in set_links:
         cat_soup = get_soup(link)
-        #print link
-        #print cat_soup
-        for prod in cat_soup.find_all('div', 'product-image '):
-            print prod
-            print('\n\n\nyoyo\n\n\n')
-            print strip_name(link, 'superstore.ca', '/plp')
-            return
-            _send_product(prod, split_name(link, 'superstore.ca', '/plp'))
-            
+        for prod in cat_soup.find_all('div', 'product-image'):
+            _send_product(prod, strip_name(link, 'superstore.ca', '/plp'), link)
 
     return
 
