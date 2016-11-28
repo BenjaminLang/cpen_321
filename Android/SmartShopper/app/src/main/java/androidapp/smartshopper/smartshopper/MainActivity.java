9db074939ca117d3a2a698ad61f763922375bcdb
JSun12 <jsun.1207@gmail.com>
2016-11-28 13:29:14 -0800

    changed cart to list

--- Android/SmartShopper/app/src/main/java/androidapp/smartshopper/smartshopper/MainActivity.java
+++ Android/SmartShopper/app/src/main/java/androidapp/smartshopper/smartshopper/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                         .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
                         .commit();
 
-                toolbar.setTitle("Shopping Cart");
+                toolbar.setTitle("Shopping List");
             }
         });
     }
