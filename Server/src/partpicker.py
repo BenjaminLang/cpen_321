dba1b06c00f7b49772d1c3f13af8030e5bfac7b3
Amrit Kooner <amrit_k729@hotmail.com>
2016-12-06 01:49:14 -0800

    finished partpicker

--- Server/src/partpicker.py
+++ Server/src/partpicker.py
@@ -1,26 +1,59 @@
 from crawl_lib import *
 
-# Takes in a soup and sends the products to the db (including price, url, name, image)
-def _send_products(link, cat_name, base_url):
-    cat_soup = get_soup(link)
-    print link
-    print cat_soup
-    exit()
-    for td in cat_soup.find_all('td', 'tdname'):
-        for prod in td.find_all('a', limit=1):
-            prod_link = base_url + prod['href']
-            prod_soup = get_soup(prod_link)
-            print prod_soup
-            exit()
+from selenium import webdriver
 
-            for title in prod_soup.find_all('div', 'title', limit=1):
-                for name in title.find_all('h1', 'name', limit=1):
-                    data['name'] = strip_name(name, '>', '</h1>')
-                    print data['name']
-                    break
+def _send_product(link, cat_name, base_url):
+    soup = get_soup(link)
+
     data = {}
 
-    #send_to_db(cat_name, data)
+    for name in soup.find_all('h1', 'name', limit=1):
+        data['name'] = strip_name(str(name), '<h1 class="name">', '</h1>')
+
+    for a in soup.find_all('a', 'item', limit=1):
+        for img in a.find_all('img', limit=1):
+            data['image'] = img['src']
+
+    for tbody in soup.find_all('tbody', limit=1):
+        for tr in tbody.find_all('tr'):
+            merchant = tr.find_all('td', 'merchant', limit=1)
+            if not merchant:
+                continue # this is a 'note' divider, not an actual store/price
+
+            for img in merchant[0].find_all('img', limit=1):
+                data['store'] = strip_name(str(img), 'alt="', '"')
+
+            for base in tr.find_all('td', 'base', limit=1):
+                for a in base.find_all('a', limit=1):
+                    data['price'] = strip_name(str(a), 'target="_blank">', '</a>')
+                    data['url'] = base_url + a['href']
+
+            print(data)
+
+    #send_to_db(...)
+
+    return
+
+# Takes in a soup and sends the products to the db (including price, url, name, image)
+def _send_products(link, cat_name, base_url):
+    print(link)
+
+    data = {} # player, quality, stuff
+
+    browser = webdriver.PhantomJS()
+    browser.set_window_size(112, 55)
+    browser.set_page_load_timeout(60)
+    browser.get(link)
+
+    prod_list = browser.find_element_by_tag_name('tbody')
+
+    prods = prod_list.find_elements_by_tag_name('tr')
+
+    for prod in prods:
+        tdname = prod.find_element_by_class_name('tdname')
+        a = tdname.find_element_by_tag_name('a')
+        prod_link = a.get_attribute('href')
+        _send_product(prod_link, cat_name, base_url)
 
     return
 
@@ -33,7 +66,6 @@ def parse():
         for cat in section.find_all('a'):
             cat_link = base_url + cat['href']
             _send_products(cat_link, strip_name(cat['href'], '/products/', '/'), base_url)
-
     return
 
 if __name__ == '__main__':
