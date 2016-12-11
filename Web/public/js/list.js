2d5812881e2a3a998ffa4df012d0c736aae8b91f
Leo Belanger <leobelanger1996@gmail.com>
2016-12-11 01:23:46 -0800

    Deleting Items from list

--- Web/public/js/list.js
+++ Web/public/js/list.js
@@ -95,7 +95,7 @@ $(document).click(function (e){
      var itemName = tag.siblings("h4").text(); 
      alert(itemName);
      for(var i = 0; i < localStorage.cart.length; i++) {
-       delete localStorage.cart[i]['itemName'];
+       delete localStorage.cart[i][name];
      }
      
      //localStorage.cart = JSON.stringify(data);
